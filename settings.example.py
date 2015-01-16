725025f15b1f4f8d6de3166855e108e228e2a642
Dave Masse <dmasse@rudemoose.com>
2015-01-15 22:38:03 -0500

    Adding sample settings file.

---
+++ settings.example.py
@@ -0,0 +1,13 @@
+NEST_USERNAME = ''
+NEST_PASSWORD = ''
+
+TWILIO_ACCOUNT_SID = ''
+TWILIO_AUTH_TOKEN = ''
+TWILIO_FROM_NUMBER = ''
+TWILIO_TO_NUMBERS = []
+
+# Temperature difference (in degrees F) after which to send notification
+TRIGGER_TEMP_DIFF = 3
+
+# The highest number of notifications to receive for any given thermostat
+MAX_NOTIFICATIONS = 2
